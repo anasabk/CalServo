b03a7a8fb0796342de817e93c2dfbf9faf974a8d
Enes <anas.abk2001@gmail.com>
2023-02-19 14:18:08 +0300

    fixing names and adding cmake files for esp-idf support as a component

---
+++ CalServo.h
@@ -0,0 +1,17 @@
+#ifndef CALSERVO_H_
+#define CALSERVO_H_
+
+class MyServo {
+private:
+    int pin;
+    double a, b;
+
+public:
+    MyServo(int servoPin);
+
+    void refreshFitter(int* pwm_list, int* degree_list, int data_len);
+
+    int getPWM(int degree);
+};
+
+#endif
